f3f22ecd44b322b11150827b6e0b4173105ff956
Abdullah <abdullahkhrief@gmail.com>
2023-03-29 15:16:57 +0300

    linters has been added

--- src/modules/edit.js
+++ src/modules/edit.js
@@ -1,54 +1,39 @@
-let event = (event) => {
-  let li = event.target.parentElement.previousSibling.previousSibling.innerHTML
-    let input = document.createElement("input")
-  input.value = li
-    input.classList.add("editor")
-  if(event.target.classList.contains("fa-sharp")){
-      event.target.classList.toggle("fa-ellipsis-vertical")
-    event.target.classList.toggle("fa-trash")
-     event.target.parentElement.previousSibling.previousSibling.replaceWith(input) 
-     }
-    
-}
-
-let p = document.querySelector(".index")
-
-let adde = (event) => {
-let book = [];
-  if(localStorage.getItem('used') === null){
-      book = [];
-  }else{
-      book = JSON.parse(localStorage.getItem("used"))
+const event = (event) => {
+  const li = event.target.parentElement.previousSibling.previousSibling.innerHTML;
+  const input = document.createElement('input');
+  input.value = li;
+  input.classList.add('editor');
+  if (event.target.classList.contains('fa-sharp')) {
+    event.target.classList.toggle('fa-ellipsis-vertical');
+    event.target.classList.toggle('fa-trash');
+    event.target.parentElement.previousSibling.previousSibling.replaceWith(input);
   }
-  
-  const value = document.querySelector(".editor")
-  const newDiv = document.createElement("div")
-  
-  newDiv.innerHTML = `${value.value}`
+};
 
-    
-if(value.value !== "" && event.target.classList.contains("editor")){
+const adde = (event) => {
+  let task = [];
+  if (localStorage.getItem('used') === null) {
+    task = [];
+  } else {
+    task = JSON.parse(localStorage.getItem('used'));
+  }
 
-event.target.nextSibling.nextSibling.firstChild.classList.toggle("fa-ellipsis-vertical");
-    event.target.nextSibling.nextSibling.firstChild.classList.toggle("fa-trash")
+  const value = document.querySelector('.editor');
+  const newDiv = document.createElement('div');
 
-  console.log(event.target.nextSibling.nextSibling.nextSibling.innerHTML)
-let targetE = event.target.nextSibling.nextSibling.nextSibling.innerHTML
-console.log(targetE + "clicked")
-  book[targetE - 1].discription = value.value
+  newDiv.innerHTML = `${value.value}`;
 
+  if (value.value !== '' && event.target.classList.contains('editor')) {
+    event.target.nextSibling.nextSibling.firstChild.classList.toggle('fa-ellipsis-vertical');
+    event.target.nextSibling.nextSibling.firstChild.classList.toggle('fa-trash');
 
-  
-event.target.replaceWith(newDiv);
+    const targetE = event.target.nextSibling.nextSibling.nextSibling.innerHTML;
+    task[targetE - 1].discription = value.value;
 
+    event.target.replaceWith(newDiv);
 
-  localStorage.setItem("used",JSON.stringify(book))
+    localStorage.setItem('used', JSON.stringify(task));
+  }
+};
 
-
-}
-
-  
-
-}
-
-export {event, adde}
+export { event, adde };
