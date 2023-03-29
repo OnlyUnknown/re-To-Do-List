e7324da159be4bd497c274d40ebc8d403a835829
Abdullah <abdullahkhrief@gmail.com>
2023-03-29 15:04:16 +0300

    Edit effect the local storage

--- src/modules/edit.js
+++ src/modules/edit.js
@@ -1,32 +1,54 @@
 let event = (event) => {
-    let li = document.querySelector(".value-list").innerHTML
-      let input = document.createElement("input")
-    input.value = li
-      input.classList.add("editor")
-    if(event.target.classList.contains("fa-sharp")){
-        event.target.classList.toggle("fa-ellipsis-vertical")
-      event.target.classList.toggle("fa-trash")
-       event.target.parentElement.previousSibling.previousSibling.replaceWith(input) 
-       }
-      
- }
+  let li = event.target.parentElement.previousSibling.previousSibling.innerHTML
+    let input = document.createElement("input")
+  input.value = li
+    input.classList.add("editor")
+  if(event.target.classList.contains("fa-sharp")){
+      event.target.classList.toggle("fa-ellipsis-vertical")
+    event.target.classList.toggle("fa-trash")
+     event.target.parentElement.previousSibling.previousSibling.replaceWith(input) 
+     }
+    
+}
 
+let p = document.querySelector(".index")
 
 let adde = (event) => {
-    
-    const value = document.querySelector(".editor")
-    const newDiv = document.createElement("div")
-    
-    newDiv.innerHTML = `${value.value}`
+let book = [];
+  if(localStorage.getItem('used') === null){
+      book = [];
+  }else{
+      book = JSON.parse(localStorage.getItem("used"))
+  }
   
-      
+  const value = document.querySelector(".editor")
+  const newDiv = document.createElement("div")
+  
+  newDiv.innerHTML = `${value.value}`
+
+    
 if(value.value !== "" && event.target.classList.contains("editor")){
-    event.target.nextSibling.nextSibling.firstChild.classList.toggle("fa-ellipsis-vertical");
-      event.target.nextSibling.nextSibling.firstChild.classList.toggle("fa-trash")
 
+event.target.nextSibling.nextSibling.firstChild.classList.toggle("fa-ellipsis-vertical");
+    event.target.nextSibling.nextSibling.firstChild.classList.toggle("fa-trash")
+
+  console.log(event.target.nextSibling.nextSibling.nextSibling.innerHTML)
+let targetE = event.target.nextSibling.nextSibling.nextSibling.innerHTML
+console.log(targetE + "clicked")
+  book[targetE - 1].discription = value.value
+
+
+  
 event.target.replaceWith(newDiv);
-}
+
+
+  localStorage.setItem("used",JSON.stringify(book))
+
 
 }
 
- export {event, adde}
+  
+
+}
+
+export {event, adde}
